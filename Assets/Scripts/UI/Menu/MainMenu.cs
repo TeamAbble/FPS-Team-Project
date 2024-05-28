2b2cf10ad387fb4822f940810b51088958792974
FIIZYLemon <matherdylan04@gmail.com>
2024-05-28 19:31:31 +0100

    BIG FAT UI PUSH
    Lots of changes to the UI ;)

---
+++ Assets/Scripts/UI/Menu/MainMenu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button startButton;
+    public Button settingsButton;
+    public Button quitButton;
+    public MenuManager menuManagerRef;
+    public string sceneName;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnEnable()
+    {
+        startButton.onClick.AddListener(StartGame);
+        settingsButton.onClick.AddListener(Settings);
+        quitButton.onClick.AddListener(Quit);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void Settings()
+    {
+        
+    }
+}
